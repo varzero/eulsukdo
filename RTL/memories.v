8b0da68647b1f8f860b0824986b0aaaa0f48c6e2
Sung min Shin <cydiazero@gmail.com>
2026-02-08 21:18:40 +0900

    fifo write 부분 작성 완료

--- RTL/memories.v
+++ RTL/memories.v
@@ -82,8 +82,34 @@ module regfile #(
 
 endmodule
 
+// (FOR FIFO_RAM)=======================================================================
+module priority_decoder #(
+    parameter       ENTRIES         = 7
+) (
+    input       [ENTRIES-1:0]                           i_entries_list, // priortiy is MSB first
+    output reg  [ENTRIES-1:0]                           o_top_entries,
+    output reg                                          o_valid
+);
+    
+    // Synthesis Variables
+    integer var_target_entry = 0;
+    
+    // Priority Decoder CL
+    always @(*) begin
+        o_top_entries = {(ENTRIES){1'b0}};
+        o_valid = 1'b0;
+        for (var_target_entry = (ENTRIES-1); var_target_entry >= 0; var_target_entry = var_target_entry - 1) begin
+            if (i_entries_list[var_target_entry]) begin
+                o_top_entries[var_target_entry] = 1'b1;
+                o_valid = 1'b1;
+            end
+        end
+    end
+
+endmodule
+
 /*
-[fifo_sram]
+[fifo_multi_chan_sram]
 ##########################################################################################
 PARAMETERIZE MULTI READ/WRITE CHANNELS FIFO, INTERNAL MEMORY IS SRAM/BRAM
 - READ LATENCY          : IMMEDIATELY
@@ -91,7 +117,7 @@ PARAMETERIZE MULTI READ/WRITE CHANNELS FIFO, INTERNAL MEMORY IS SRAM/BRAM
 
 LOGICAL  
 
-* TESTBENCH MODULE      : tb_fifo_sram
+* TESTBENCH MODULE      : tb_fifo_multi_chan_sram
 ------------------------------------------------------------------------------------------
 [PARAMETER(USER MODIFY)]
     READ_CHANNEL        : NUMBER OF READ CHANNELS
@@ -102,12 +128,13 @@ LOGICAL
 [INPUT/OUTPUT]
     clk                 : [INPUT ] SYSTEM CLOCK
     reset_n             : [INPUT ] SYSTEM ACTIVE-LOW RESET
+    i_read_get          : [INPUT ] READ ENTRIES FROM READ CHANNELS
     i_write_wes         : [INPUT ] WRITE ENABLE OF WRITE CHANNELS
     i_write_data        : [INPUT ] DATA OF WRITE CHANNELS
     o_read_data         : [OUTPUT] DATA OF READ CHANNELS
 ##########################################################################################
 */
-module fifo_sram #(
+module fifo_multi_chan_sram #(
     parameter       READ_CHANNEL    = 8 ,
     parameter       WRITE_CHANNEL   = 4 ,
     parameter       ENTRIES         = 16,
@@ -116,23 +143,73 @@ module fifo_sram #(
 ) (
     input                                               clk                 ,
     input                                               reset_n             ,
-    input                                               i_read_new          ,
+    input       [READ_CHANNEL-1:0]                      i_read_get          ,
     input       [WRITE_CHANNEL-1:0]                     i_write_wes         ,
     input       [WRITE_CHANNEL*REG_WIDTH-1:0]           i_write_data        ,
     output reg  [READ_CHANNEL*REG_WIDTH-1:0]            o_read_data
 );
+    // Synthesis Variables
+    integer var_read_entry_idx = 0;
+    integer var_read_data_bit_position = 0;
+    integer var_write_entry_idx = 0;
+    integer var_write_data_bit_position = 0;
+    
     // Constant
-    localparam INTERNAL_ADDR_WIDTH = (ENTRIES/WRITE_CHANNEL);
+    localparam INTERNAL_DEPTH = ENTRIES / WRITE_CHANNEL;
+    localparam INTERNAL_ADDR_WIDTH = $clog2(INTERNAL_DEPTH);
     localparam INTERNAL_DATA_WIDTH = (REG_WIDTH*WRITE_CHANNEL);
 
     // SRAM/BRAM Control Value: SYNTHESIS => WIRES OR COMBINATIONAL LOGIC
-    reg ram_we; // CL
-    reg [INTERNAL_ADDR_WIDTH-1:0] ram_addr;
-    wire [INTERNAL_DATA_WIDTH-1:0] ram_data_o;
+    reg ram_we, ram_we_next;
+    reg [INTERNAL_ADDR_WIDTH-1:0] ram_addr, ram_addr_next;
+    wire [INTERNAL_DATA_WIDTH-1:0] ram_data_insert, ram_data_insert_next;
 
     // DATAPATH Registers
-    reg [RAM_DATA_WIDTH-1:0] buf_read, buf_read_next;
-    reg [RAM_DATA_WIDTH-1:0] buf_write, buf_write_next;
+    reg [INTERNAL_DATA_WIDTH-1:0] buf_read, buf_read_next;
+    reg [INTERNAL_DATA_WIDTH-1:0] buf_write, buf_write_next;
 
     // Registers MODELING
+    always @(posedge clk or negedge reset_n) begin
+        if (reset_n == 0) begin
+            ram_we <= 1'b0;
+            ram_addr <= 0;
+            ram_data_insert <= 0;
+
+            buf_read <= 0;
+            buf_write <= 0;
+        end
+        else begin
+            ram_we <= ram_we_next;
+            ram_addr <= ram_addr_next;
+            ram_data_insert <= ram_data_insert_next;
+
+            buf_read <= buf_read_next;
+            buf_write <= buf_write_next;
+        end
+    end
+
+    // Read System MODELING ( COMBINATIONAL LOGIC )
+    always @(*) begin
+        o_read_data = '0;
+        var_read_data_bit_position = 0;
+        
+        for (var_read_entry_idx = 0; var_read_entry_idx < READ_CHANNEL; var_read_entry_idx = var_read_entry_idx + 1) begin
+            if (i_read_get[0]) begin
+                o_read_data[var_read_data_bit_position +: REG_WIDTH] = buf_read[];
+            end
+        end
+    end
+
+    // Write System MODELING ( COMBINATIONAL LOGIC )
+    always @(*) begin
+        buf_write_next = '0;
+        var_write_data_bit_position = 0;
+        ram_we_next = |i_write_wes;
+        for (var_write_entry_idx = 0; var_write_entry_idx < WRITE_CHANNEL; var_write_entry_idx = var_write_entry_idx + 1) begin
+            if (i_write_wes[var_write_entry_idx]) begin
+                buf_write_next[var_write_data_bit_position +: REG_WIDTH] = i_write_data[(var_write_entry_idx*REG_WIDTH) +: REG_WIDTH];
+                var_write_data_bit_position = var_write_data_bit_position + REG_WIDTH;
+            end
+        end
+    end
 endmodule
