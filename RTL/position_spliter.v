1bd0adc62051c259e0ba0f051a5d7a9a9c2422be
Sung min Shin <cydiazero@gmail.com>
2026-03-18 00:16:25 +0900

    entrynum을 위한 멀티 채널 입출력 FIFO 구조 설계 완.

--- RTL/position_spliter.v
+++ RTL/position_spliter.v
@@ -1,4 +1,4 @@
-module position_spliter #(
+module position_splitter #(
 	parameter INPUT_ENTRIES = 5,
 	parameter DATA_WIDTH = 32,
 	parameter BIT_WIDTH_INPUT_ENTRIES = $clog2(INPUT_ENTRIES)
@@ -8,7 +8,8 @@ module position_spliter #(
 	output reg [INPUT_ENTRIES-1:0] out_position_o,
 	output reg [(INPUT_ENTRIES*DATA_WIDTH)-1:0] data_o
 );
-	integer out_position_last, check_position_target;	
+	reg [BIT_WIDTH_INPUT_ENTRIES-1:0] out_position_last;
+	integer check_position_target;	
 
 	always @(*) begin
 		out_position_o = 0; data_o = 0; 
@@ -44,6 +45,14 @@ module fifo_ordering_position #(
 	localparam FIFO_WIDTH = FIFO_IO_ENTRIES * ENTRY_WIDTH;
 	localparam READY_PUSH_PS_ENTRIES = FIFO_IO_ENTRIES + PUSH_DATA;
 	localparam READY_PUSH_PS_WIDTH = FIFO_WIDTH + (PUSH_DATA * ENTRY_WIDTH);
+	localparam READY_POP_PS_ENTRIES = FIFO_IO_ENTRIES + POP_DATA;
+	localparam READY_POP_PS_WIDTH = FIFO_WIDTH + (POP_DATA * ENTRY_WIDTH);
+
+	// Registers
+	reg  [READY_PUSH_PS_ENTRIES-1:0] push_fifoready_valid_reg;
+	reg  [READY_PUSH_PS_WIDTH-1:0] push_fifoready_data_reg;
+	reg  [READY_POP_PS_ENTRIES-1:0] pop_out_valid_reg;
+	reg  [READY_POP_PS_WIDTH-1:0] pop_out_data_reg;
 
 	// for U_PS_PUSH and U_PS_PUSH_2_FIFO
 	wire [PUSH_DATA-1:0] push_ordering_valid;
@@ -57,31 +66,79 @@ module fifo_ordering_position #(
 	reg  [READY_PUSH_PS_WIDTH-1:0] push_fifoready_new_data;
 	reg	 push_fifo_we;
 	reg  [FIFO_WIDTH-1:0] push_fifo_data;
-	always @(*) begin // comb logic
-		push_fifo_we = &( push_fifoready_valid[FIFO_IO_ENTRIES-1:0] );
-		push_fifo_data = push_fifoready_data[FIFO_WIDTH-1:0];
 
-		if (push_fifo_we) begin
+	// for U_INTERNAL_FIFO and U_PS_FIFO_2_OUT
+	wire fifo_empty;
+	wire [FIFO_WIDTH-1:0] pop_fifoout_data;
+	wire [READY_POP_PS_ENTRIES-1:0] pop_out_new_valid;
+	wire [READY_POP_PS_WIDTH-1:0] pop_out_new_data;
+	reg  fifo_get_position;
+	reg  [READY_POP_PS_ENTRIES-1:0] pop_out_valid;
+	reg  [READY_POP_PS_WIDTH-1:0] pop_out_data;
+
+	always @(*) begin // comb logic
+		// PUSH SECTION
+			// FIFO 부분 채워졌는지 결정
+		push_fifo_we = &( push_fifoready_valid_reg[FIFO_IO_ENTRIES-1:0] );
+		push_fifo_data = push_fifoready_data_reg[FIFO_WIDTH-1:0];
+
+		if (push_fifo_we) begin // FIFO 부분 채워짐
 			push_fifoready_new_valid = {push_ordering_valid, {READY_PUSH_PS_ENTRIES{1'b0}} };
 			push_fifoready_new_data = {push_ordering_data, {READY_PUSH_PS_WIDTH{1'b0}} };
 		end
+		else begin // FIFO 부분 아직 안채워짐
+			push_fifoready_new_valid = {push_ordering_valid, 
+										push_fifoready_valid_reg[FIFO_IO_ENTRIES-1:0]};
+			push_fifoready_new_data  = {push_ordering_data, 
+										push_fifoready_data_reg[FIFO_WIDTH-1:0]};
+		end
+
+		// POP SECTION
+		if ( |pop_out_valid_reg[READY_POP_PS_ENTRIES-1:POP_DATA] ) begin
+			// 가져온것이 아직 남아있는 경우: 유지
+			fifo_get_position = 0;
+			pop_out_valid = pop_out_valid_reg;
+			pop_out_data = pop_out_data_reg;
+		end
 		else begin
-			push_fifoready_new_valid = {push_ordering_valid, push_fifoready_valid};
-			push_fifoready_new_data = {push_ordering_data, push_fifoready_data};
+			// 새로 보충해야 하는 경우
+			if (~fifo_empty) begin // FIFO의 데이터가 있는 경우
+				fifo_get_position = 1'b1;
+				pop_out_valid = {{FIFO_IO_ENTRIES{1'b1}}, pop_out_valid_reg[POP_DATA-1:0]};
+				pop_out_data = {pop_fifoout_data, pop_out_data_reg[POP_DATA-1:0]};
+			end
+			else begin // FIFO에 데이터가 없는 경우
+				fifo_get_position = 0;
+				pop_out_valid = {push_fifoready_valid_reg[FIFO_IO_ENTRIES-1:0], 
+								 pop_out_valid_reg[POP_DATA-1:0]};
+				pop_out_data  = {push_fifo_data, 
+								 pop_out_data_reg[POP_DATA-1:0]};
+
+				// PUSH 부분에서 FIFO 부분은 지우기
+				push_fifo_we = 0;
+				push_fifoready_new_valid = {push_ordering_valid, {READY_PUSH_PS_ENTRIES{1'b0}} };
+				push_fifoready_new_data = {push_ordering_data, {READY_PUSH_PS_WIDTH{1'b0}} };
+			end
 		end
 	end
 
-	// for U_INTERNAL_FIFO and U_PS_FIFO_2_OUT
-	//
-
+	// Registers Modeling
 	always @(posedge clk or negedge reset_n) begin
 		if (reset_n == 1'b0) begin
+			push_fifoready_valid_reg <= 0;
+			push_fifoready_data_reg <= 0;
+			pop_out_valid_reg <= 0;
+			pop_out_data_reg <= 0;
 		end
 		else begin
+			push_fifoready_valid_reg <= push_fifoready_valid;
+			push_fifoready_data_reg <= push_fifoready_data;
+			pop_out_valid_reg <= pop_out_new_valid;
+			pop_out_data_reg <= pop_out_new_data;
 		end
 	end
 
-	position_spliter #(
+	position_splitter #(
 		.INPUT_ENTRIES(PUSH_DATA),
 		.DATA_WIDTH(ENTRY_WIDTH)
 	) U_PS_PUSH (
@@ -91,7 +148,7 @@ module fifo_ordering_position #(
 		.data_o				(push_ordering_data)
 	);
 
-	position_spliter #(
+	position_splitter #(
 		.INPUT_ENTRIES(FIFO_IO_ENTRIES),
 		.DATA_WIDTH(ENTRY_WIDTH)
 	) U_PS_PUSH_2_FIFO (
@@ -101,30 +158,29 @@ module fifo_ordering_position #(
 		.data_o				(push_fifoready_data)
 	);
 
+	// FIFO는 한 FIFO 라인이 모두 채워지면 그때 저장함
     fifo_sram #(
 		.ENTRIES(FIFO_DEPTH), 
 		.REG_WIDTH(FIFO_WIDTH)
 	) U_INTERNAL_FIFO (
         .clk                 (clk),
         .reset_n             (reset_n),
-        .i_read_get          (),
+        .i_read_get          (fifo_get_position),
         .i_write_we          (push_fifo_we),
         .i_write_data        (push_fifo_data),
-        .o_read_data         (),
-        .o_empty             (),
+        .o_read_data         (pop_fifoout_data),
+        .o_empty             (fifo_empty),
         .o_full              ()
     );
 
-	position_spliter #(
+	position_splitter #(
 		.INPUT_ENTRIES(FIFO_IO_ENTRIES),
 		.DATA_WIDTH(ENTRY_WIDTH)
 	) U_PS_FIFO_2_OUT (
-		.valid_position_i	(),
-		.position_data_i	(),
-		.out_position_o		(),
-		.data_o				()
+		.valid_position_i	(pop_out_valid),
+		.position_data_i	(pop_out_data),
+		.out_position_o		(pop_out_new_valid),
+		.data_o				(pop_out_new_data)
 	);
-
-
 
 endmodule
