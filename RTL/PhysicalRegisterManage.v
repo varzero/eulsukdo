0321dacbf8bf57a326d651df6efa7ae21285210e
Sung min Shin <cydiazero@gmail.com>
2026-03-18 11:08:03 +0900

    allocator 추가

--- RTL/PhysicalRegisterManage.v
+++ RTL/PhysicalRegisterManage.v
@@ -1,11 +1,17 @@
 module prm #(
 	parameter NUM_OF_PHY_REGS = 64,
 	parameter ROB_ENTRIES = 128,
-    parameter NUM_OF_WB_ENTRIES = 5,
 	parameter OPREANDS = 2,
+    parameter NUM_OF_NEW_ENTRIES = 2,
+    parameter NUM_OF_WB_ENTRIES = 5,
+	parameter BUF_ENTRIES_PER_PHYREG = 4,
+    parameter SRAM_SIZE = 128,
+	parameter PHYREG_ADDR_WIDTH = $clog2(NUM_OF_PHY_REGS),
+	parameter SRAM_ADDR_WIDTH = $clog2(SRAM_SIZE)
 ) (
     input clk,
     input reset_n,
 );
+
 
 endmodule
--- RTL/ReadyStation.v
+++ RTL/ReadyStation.v
@@ -2,7 +2,7 @@ module rs #( // Ready Station
 	parameter IN_ENTRIES = 5,
 	parameter NUM_OF_EX = 5,
 	parameter ROB_ENTRIES = 128,
-	parameter MICRO_OP_LENGHT = 5,
+	parameter MICRO_OP_LENGHT = 5, // LSB 쪽으로 EX_SPECIFY_WIDTH 만큼은 EX 선택용
 	parameter NUM_OF_PHY_REGS = 64,
 	parameter OPREANDS = 2,
 	parameter FIFO_ENTRIES = 32,
@@ -20,6 +20,8 @@ module rs #( // Ready Station
 	output [(NUM_OF_EX * RS_ENTRY_WIDTH)-1:0] ex_fifo_data_o,
 	output fifo_full_block
 );
+	reg [RS_ENTRY_WIDTH-1:0] entry_one;
+	reg [MICRO_OP_LENGHT-1:0] microop_inst
 	reg [IN_ENTRIES-1:0] input_valid_position [0:NUM_OF_EX-1];
 	wire [IN_ENTRIES-1:0] position_ordering [0:NUM_OF_EX-1]; 
 	wire [(IN_ENTRIES * RS_ENTRY_WIDTH)-1:0] data_ordering [0:NUM_OF_EX-1]; 
@@ -33,20 +35,20 @@ module rs #( // Ready Station
 
 	generate
 		for (ex_path = 0; ex_path < NUM_OF_EX; ex_path = ex_path+1) begin
-			position_spliter U_RS_IN_PSP #(
+			position_spliter #(
 					.INPUT_ENTRIES(IN_ENTRIES),
 					.DATA_WIDTH(RS_ENTRY_WIDTH)
-				) (
+				) U_RS_IN_PSP (
 					.valid_position_i(input_valid_position[ex_path]),
 					.position_data_i(entry_data_i),
 					.out_position_o(position_ordering[ex_path])
 					.data_o(data_ordering[ex_path])
 				);
 
-			fifo_sram U_RS_FIFO #(
+			fifo_sram #(
     			.ENTRIES            ((FIFO_ENTRIES / IN_ENTRIES) + (((FIFO_ENTRIES % IN_ENTRIES) == 0)? 0 : 1)),
     			.REG_WIDTH          (IN_ENTRIES * RS_ENTRY_WIDTH)
-			) (
+			) U_RS_FIFO (
 			    .clk                (clk),
 			    .reset_n            (reset_n),
 			    .i_read_get         (ex_busy_i[ex_path]),
@@ -63,10 +65,11 @@ module rs #( // Ready Station
 		input_valid_position = 0;
 		for (valid_check = 1; valid_check <= NUM_OF_EX, valid_check = valid_check + 1) begin
 			for (check_target = 0; check_target <= IN_ENTRIES, check_target = check_target + 1) begin
-				if ( ex_target_i[( ((IN_ENTRIES * EX_SPECIFY_WIDTH) * (valid_check-1)) 
-									+ (EX_SPECIFY_WIDTH * check_target) ) 
-								+: EX_SPECIFY_WIDTH] 
-					&& valid_check )
+				entry_one = ex_target_i[( ((IN_ENTRIES * EX_SPECIFY_WIDTH) * (valid_check-1)) 
+											+ (EX_SPECIFY_WIDTH * check_target) ) 
+										+: EX_SPECIFY_WIDTH];
+				microop_inst = entry_one[ROB_ADDR_WIDTH +: MICRO_OP_LENGHT];
+				if ( microop_inst && valid_check )
 				begin
 					input_valid_position[velid_check-1][check_target] = 1'b1;
 				end
