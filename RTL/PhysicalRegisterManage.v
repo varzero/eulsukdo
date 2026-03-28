c45729c4c6053917f64c71c10ccaf479f9de1713
Sung min Shin <cydiazero@gmail.com>
2026-03-28 20:06:09 +0900

    PRM 구조 작성완

--- RTL/PhysicalRegisterManage.v
+++ RTL/PhysicalRegisterManage.v
@@ -15,5 +15,76 @@ module prm #(
     input reset_n,
 );
 
+	regfile U_PHY_REG_ROB_MAPPING_RF #(
+    	.READ_CHANNEL    (),
+    	.WRITE_CHANNEL   (),
+    	.ENTRIES         (),
+    	.REG_WIDTH       (),
+	) (
+	    .clk                 (),
+	    .reset_n             (),
+	    .i_read_addresses    (),
+	    .i_write_wes         (),
+	    .i_write_addresses   (),
+	    .i_write_data        (),
+	    .o_read_data		 ()
+	);
+
+	allocator_start_one U_UNALLOCATE_PHY_REG_ALLOCATOR #(
+		.NUM_OF_ENTRIES (),
+    	.UNALLOCATES 	(),
+    	.ALLOCATES 		()
+	) (
+	    .clk					(),
+	    .reset_n				(),
+	    .unallocate_valid_i		(),
+	    .unallocate_entries_i	(),
+	    .allocating_i			(),
+		.allocate_valid_o		(),
+	    .allocate_entries_o		(),
+		.init_done				()
+	);
+
+	regfile U_PRRM_LIST_BUF #(
+    	.READ_CHANNEL    (),
+    	.WRITE_CHANNEL   (),
+    	.ENTRIES         (),
+    	.REG_WIDTH       (),
+	) (
+	    .clk                 (),
+	    .reset_n             (),
+	    .i_read_addresses    (),
+	    .i_write_wes         (),
+	    .i_write_addresses   (),
+	    .i_write_data        (),
+	    .o_read_data		 ()
+	);
+
+	allocator U_PRRM_LIST_SRAM_ADDR_ALLOCATOR #(
+		.NUM_OF_ENTRIES (),
+    	.UNALLOCATES 	(),
+    	.ALLOCATES 		()
+	) (
+	    .clk					(),
+	    .reset_n				(),
+	    .unallocate_valid_i		(),
+	    .unallocate_entries_i	(),
+	    .allocating_i			(),
+		.allocate_valid_o		(),
+	    .allocate_entries_o		(),
+		.init_done				()
+	);
+	
+	on_chip_sync_dual_port_ram U_PRRM_LIST_SRAM #(
+	    .ENTRIES         (),
+	    .ENTRY_WIDTH     ()
+	) (
+	    .clk	(),
+	    .r_addr	(),
+	    .we		(),
+	    .w_addr	(),
+	    .w_data	(),
+	    .r_data	()
+	);
 
 endmodule
