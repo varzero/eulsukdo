689577f7c06b24d88acf6d145d2a5c766b5c56bb
Sung min Shin <cydiazero@gmail.com>
2026-04-01 16:52:46 +0900

    PRM I/O 작성시작

--- RTL/PhysicalRegisterManage.v
+++ RTL/PhysicalRegisterManage.v
@@ -5,14 +5,33 @@ module prm #(
     
     parameter NUM_OF_NEW_ENTRIES = 2,
     parameter NUM_OF_WB_ENTRIES = 5,
+	parameter NUM_OF_DESTROY_ENTRIES = 4,
 	parameter BUF_ENTRIES_PER_PHYREG = 4,
+	parameter OPREANDS = 2,
 
     parameter SRAM_ENTRIES_SIZE = 128,
-	parameter PHYREG_ADDR_WIDTH = $clog2(NUM_OF_PHY_REGS),
-	parameter SRAM_ADDR_WIDTH = $clog2(SRAM_SIZE)
+	parameter SRAM_ADDR_WIDTH = $clog2(SRAM_SIZE),
+	parameter IST_ADDR_WIDTH = $clog2(IST_ENTRIES),
+	parameter PHYREG_ADDR_WIDTH = $clog2(NUM_OF_PHY_REGS)
 ) (
     input clk,
     input reset_n,
+
+	// Allocate PHYREG
+	input [NUM_OF_NEW_ENTRIES-1:0] allocate_phyreg_get_i,
+	output [NUM_OF_NEW_ENTRIES-1:0] allocate_phyreg_valid_o,
+	output [(NUM_OF_NEW_ENTRIES * PHYREG_ADDR_WIDTH)-1:0] allocate_phyregs_o,
+
+	// Unallocate PHYREG
+	input [NUM_OF_DESTROY_ENTRIES-1:0] unallocate_phyreg_valid_i,
+	input [(NUM_OF_DESTROY_ENTRIES * PHYREG_ADDR_WIDTH)-1:0] unallocate_phyregs_i,
+
+	// PHYREG Use IST Mapping
+	input [(NUM_OF_NEW_ENTRIES * OPREANDS)-1:0] target_ist_valid_i,
+	input [(NUM_OF_NEW_ENTRIES * IST_ADDR_WIDTH)-1:0] target_ist_entry_i,
+	input [((NUM_OF_NEW_ENTRIES * OPREANDS) * PHYREG_ADDR_WIDTH)-1:0] target_phyregs_i,
+
+
 );
 
 	regfile #(
